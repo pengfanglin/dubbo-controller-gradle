bf46666770d00eaae71b4f74344447c9b222d8ed
pengfanglin <2639331561@qq.com>
2019-07-23 00:28:54 +0800

    /

--- src/main/java/com/fanglin/dubbo/controller/MemberController.java
+++ src/main/java/com/fanglin/dubbo/controller/MemberController.java
@@ -7,6 +7,10 @@ import com.fanglin.common.core.page.PageResult;
 import com.fanglin.common.utils.WxUtils;
 import com.fanglin.dubbo.service.MemberServiceI;
 import com.fanglin.dubbo.template.model.MemberModel;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,15 +26,22 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @RestController
 @RequestMapping("/member/")
+@Api(value = "/member/", tags = {"用户"})
 public class MemberController {
     @Autowired
     MemberServiceI memberService;
 
+
+    @ApiOperation("测试")
+    @ApiImplicitParams({
+        @ApiImplicitParam(name = "id", value = "账号id", required = true)
+    })
     @PostMapping("test")
     public Ajax test() {
         return Ajax.ok(WxUtils.getJsApiTicket("555"));
     }
 
+    @ApiOperation("测试")
     @PostMapping("updateMember")
     public Ajax updateMember(MemberModel member) {
         if (memberService.updateMember(member) > 0) {
